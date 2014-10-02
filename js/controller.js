6d77e41c41d07b2e9d456e64c3ce5f3555e6681d
Ronald-Baars <Ronald.baars@hva.nl>
2014-10-02 23:29:00 +0200

    Tekstpagina's toegevoegd

--- js/controller.js
+++ js/controller.js
@@ -41,19 +41,24 @@ MOVIEWEB = MOVIEWEB || {};
 					MOVIEWEB.loadSection.genres();
 					MOVIEWEB.loadSection.movies(genreID);
 					MOVIEWEB.loadSection.details(movieID);
+				},
+
+				'/about': function () {
+					//text-pagina: about
+					MOVIEWEB.loadSection.genres();
+					MOVIEWEB.renderHtml.textPage("about");
+				},
+
+				'/contact': function () {
+					//text-pagina: contact
+					MOVIEWEB.loadSection.genres();
+					MOVIEWEB.renderHtml.textPage("contact");
+				},
+
+				'*': function () {
+					MOVIEWEB.loadSection.genres();
+					MOVIEWEB.renderHtml.textPage("notfound");
 				}
-
-				// '/about': function () {
-				// 	//text-pagina: about
-				// },
-
-				// '/contact': function () {
-				// 	//text-pagina: contact
-				// },
-
-				// '*': function () {
-					
-				// }
 			});
 		}
 	};
@@ -63,6 +68,7 @@ MOVIEWEB = MOVIEWEB || {};
 			if(document.querySelector("#genres ul").innerHTML === "") {
 				console.log("Genres are being loaded...");
 				MOVIEWEB.getJson.genres();
+				document.getElementById('genres').className = "animated fadeInLeft";
 			}
 		},
 		movies: function(genreID){
@@ -70,12 +76,14 @@ MOVIEWEB = MOVIEWEB || {};
 				console.log("Movies are being loaded...");
 				MOVIEWEB.getJson.movies(genreID);
 				MOVIEWEB.router.currentGenre = genreID;
+				document.getElementById('movies').className = "animated fadeInLeft";
 			}
 		},
 		details: function(movieID){
 			console.log("Details are being loaded...");
 			MOVIEWEB.getJson.details(movieID);
 			MOVIEWEB.router.currentMovie = movieID;
+			document.getElementById('details').className = "animated fadeIn";
 		}
 	};
 
@@ -127,7 +135,22 @@ MOVIEWEB = MOVIEWEB || {};
 					console.error(xhr);
 				}
 			);
+			
+			/*    BEGINSEL INFINITE SCROLLING
+			console.log("er passen: " + Math.ceil(  (window.innerHeight-60) / 34));
 
+
+			var elem = document.getElementById("movies");
+			if(elem.length > 19) {
+				console.log("offsetHeight: "+elem.offsetHeight+"  | scrollHeight: "+elem.scrollHeight);
+				
+				while(elem.offsetHeight < elem.scrollHeight) {
+					console.log("meer content laden!");
+					
+					
+				}
+			}
+		*/	
 		},
 		details: function (id) {
 			//Grab the list of genres
@@ -149,7 +172,6 @@ MOVIEWEB = MOVIEWEB || {};
 					console.error(xhr);
 				}
 			);
-
 		}
 	};
 
@@ -271,6 +293,36 @@ MOVIEWEB = MOVIEWEB || {};
 					}
 				}
 			);
+		},
+		textPage: function (page) {
+			console.log("Current page: " + page);
+			var background = document.getElementById("textPage");
+			var lightbox = document.getElementById("lightbox");
+			var close = document.getElementById("close");
+
+			close.addEventListener("click", function(){
+				background.className = "animated fadeOut";
+				lightbox.className = "animated fadeOutDown";
+				window.location.href = "#/";
+
+				setTimeout(function(){
+					background.className = "hidden";
+					lightbox.className = "hidden";
+				}, 1000);
+			});
+
+			background.className = "animated fadeIn";
+			lightbox.className = "animated fadeInDown";
+						
+			var directives = {
+				_content: {
+					html: function () {
+						return this._content;
+					}
+				}
+			};
+			Transparency.render(background, MOVIEWEB.dataBase.html[page], directives);
+
 		}
 	};
 
@@ -313,16 +365,16 @@ MOVIEWEB = MOVIEWEB || {};
 	MOVIEWEB.dataBase = {
 		html: {
 			notfound: {
-				title: "404 Error",
-				content: "Deze pagina is niet gevonden."
+				_title: "404 Error",
+				_content: "Deze pagina is niet gevonden."
 			},
 			about: {
-				title: "About this app",
-				content: "<p>Cities fall but they are rebuilt. heroes die but they are remembered. the man likes to play chess; let's get him some rocks. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. bruce... i'm god. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all. rehabilitated? well, now let me see. you know, i don't have any idea what that means. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. rehabilitated? well, now let me see. you know, i don't have any idea what that means. cities fall but they are rebuilt.</p><p><img src='img/popcorn.jpg'> heroes die but they are remembered. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all.I did the same thing to gandhi, he didn't eat for three weeks. bruce... i'm god. cities fall but they are rebuilt. heroes die but they are remembered. i once heard a wise man say there are no perfect men. only perfect intentions. cities fall but they are rebuilt. heroes die but they are remembered. boxing is about respect. getting it for yourself, and taking it away from the other guy. well, what is it today? more spelunking? let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. bruce... i'm god. well, what is it today? more spelunking? it only took me six days. same time it took the lord to make the world. i did the same thing to gandhi, he didn't eat for three weeks. Let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. boxing is about respect.</p><p>getting it for yourself, and taking it away from the other guy. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. you measure yourself by the people who measure themselves by you. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. you measure yourself by the people who measure themselves by you. you measure yourself by the people who measure themselves by you. that tall drink of water with the silver spoon up his ass. i once heard a wise man say there are no perfect men. only perfect intentions. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. boxing is about respect. getting it for yourself, and taking it away from the other guy. <p><p>That tall drink of water with the silver spoon up his ass. well, what is it today? more spelunking? i now issue a new commandment: thou shalt do the dance. let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. i did the same thing to gandhi, he didn't eat for three weeks. the man likes to play chess; let's get him some rocks. i now issue a new commandment: thou shalt do the dance. i now issue a new commandment: thou shalt do the dance. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. i don't think they tried to market it to the billionaire, spelunking, base-jumping crowd.that tall drink of water with the silver spoon up his ass. it only took me six days. same time it took the lord to make the world.</p>"
+				_title: "About this app",
+				_content: "<p>Cities fall but they are rebuilt. heroes die but they are remembered. the man likes to play chess; let's get him some rocks. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. bruce... i'm god. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all. rehabilitated? well, now let me see. you know, i don't have any idea what that means. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. rehabilitated? well, now let me see. you know, i don't have any idea what that means. cities fall but they are rebuilt.</p><p><img src='img/popcorn.jpg'> heroes die but they are remembered. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all.I did the same thing to gandhi, he didn't eat for three weeks. bruce... i'm god. cities fall but they are rebuilt. heroes die but they are remembered. i once heard a wise man say there are no perfect men. only perfect intentions. cities fall but they are rebuilt. heroes die but they are remembered. boxing is about respect. getting it for yourself, and taking it away from the other guy. well, what is it today? more spelunking? let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. bruce... i'm god. well, what is it today? more spelunking? it only took me six days. same time it took the lord to make the world. i did the same thing to gandhi, he didn't eat for three weeks. Let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. boxing is about respect.</p><p>getting it for yourself, and taking it away from the other guy. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. you measure yourself by the people who measure themselves by you. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. you measure yourself by the people who measure themselves by you. you measure yourself by the people who measure themselves by you. that tall drink of water with the silver spoon up his ass. i once heard a wise man say there are no perfect men. only perfect intentions. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. boxing is about respect. getting it for yourself, and taking it away from the other guy. <p><p>That tall drink of water with the silver spoon up his ass. well, what is it today? more spelunking? i now issue a new commandment: thou shalt do the dance. let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. i did the same thing to gandhi, he didn't eat for three weeks. the man likes to play chess; let's get him some rocks. i now issue a new commandment: thou shalt do the dance. i now issue a new commandment: thou shalt do the dance. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. i don't think they tried to market it to the billionaire, spelunking, base-jumping crowd.that tall drink of water with the silver spoon up his ass. it only took me six days. same time it took the lord to make the world.</p>"
 			},
 			contact: {
-				title: "Contact",
-				content: "Dit is de Contact-pagina."
+				_title: "Contact",
+				_content: "Dit is de Contact-pagina."
 			},
 		}
 	};
